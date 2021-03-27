c1ad1d8d36a2936d6888148e80aea5c006c769f1
Sirirak Witoon <Sirirak.b5963898@gmail.com>
2021-03-27 12:38:08 +0700

    add Header

--- src/App.js
+++ src/App.js
@@ -3,8 +3,19 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="bg-red-600">
-      <h1 className="text-red-600"> Hello </h1>{" "}
+    <div className="">
+      <div className="flex flex-row justify-between px-8 pt-10 ">
+        <div className="flex font-bold">
+          <h1 className="text-yellow-500">Sirirak </h1>
+          <h1 className="ml-2">Witoon</h1>
+        </div>
+        <div className="grid grid-cols-4 gap-4 font-bold">
+          <h1>Home</h1>
+          <h1>Resume</h1>
+          <h1>Works</h1>
+          <h1>Contacts</h1>{" "}
+        </div>
+      </div>
     </div>
   );
 }
